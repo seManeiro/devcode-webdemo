d22fcb4ab025cc5624dcf4c0f8d4cb56ddbf22fc
seManeiro <sebastian.maneiro@gmail.com>
2014-10-21 15:55:02 +0200

    bank solution not yet...

--- src/com/devcode/spring/web/service/PaymentService.java
+++ src/com/devcode/spring/web/service/PaymentService.java
@@ -1,11 +1,14 @@
 package com.devcode.spring.web.service;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.TypeReference;
 import org.springframework.stereotype.Service;
@@ -30,35 +33,36 @@ public class PaymentService {
 		String json = gson.toJson(customerCreditcard);
 		System.out.println(json);
 
-		WebResource webResource = client
-				.resource("http://dev2.devcode.se/paymentiq/api/creditcard/deposit/process");
+		WebResource webResource = client.resource("http://dev2.devcode.se/paymentiq/api/creditcard/deposit/process");
 
-		ClientResponse response = webResource.type(
-				MediaType.APPLICATION_JSON_TYPE).post(ClientResponse.class,
-				json);
+		ClientResponse response = webResource.type(MediaType.APPLICATION_JSON_TYPE).post(ClientResponse.class,json);
+		System.out.println(response.getProperties().toString());
 
 		String status = response.getEntity(String.class);
 		System.out.println(status);
 		
-		CreditcardRest ccr = gson.fromJson(status, CreditcardRest.class);
-		
-		System.out.println();
-	
-//		Map<String, String> map = new HashMap<String, String>();
-//
-//		ObjectMapper mapper = new ObjectMapper();
-//
-//		try {
-//
-//			map = mapper.readValue(status, CreditcardRest creditcardRest);
-//
-//			System.out.println(map.containsValue(true));
-//
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-		return false;
+		   ObjectMapper mapper = new ObjectMapper();
 
+		   try {
+			@SuppressWarnings("unchecked")
+			Map<String,String> state = mapper.readValue(status, Map.class);
+			
+			boolean success = state.containsValue(true);
+
+			return success;
+			
+		} catch (JsonParseException e) {
+			
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}	   
+		   
+		   return false;
 	}
 
 	public String bankPayment(CustomerBank customerBank) {
@@ -67,38 +71,40 @@ public class PaymentService {
 		String json = gson.toJson(customerBank);
 		System.out.println(json);
 
-		WebResource webResource = client
-				.resource("http://dev2.devcode.se/paymentiq/api/bank/deposit/process");
+		WebResource webResource = client.resource("http://dev2.devcode.se/paymentiq/api/bank/deposit/process");
 
-		ClientResponse response = webResource.type(
-				MediaType.APPLICATION_JSON_TYPE).post(ClientResponse.class,
-				json);
-
-		Boolean success = false;
-
-		System.out.println(response.getHeaders().containsValue(success));
-
-		System.out.println(success);
+		ClientResponse response = webResource.type(MediaType.APPLICATION_JSON_TYPE).post(ClientResponse.class, json);
 
 		String status = response.getEntity(String.class);
+		System.out.println(status);
+		
+		   ObjectMapper mapper = new ObjectMapper();
 
-		Map<String, String> map = new HashMap<String, String>();
+		   try {
+			@SuppressWarnings("unchecked")
+			Map<String,String> state = mapper.readValue(status, Map.class);
+			
+			String redirectOutput = null;
+			
+			redirectOutput = state.toString();
+			
+			System.out.println(redirectOutput);
 
-		ObjectMapper mapper = new ObjectMapper();
-
-		try {
-
-			map = mapper.readValue(status, new TypeReference<HashMap<String, String>>() {
-					});
-
-			System.out.println(map.containsValue(true));
-
-		} catch (Exception e) {
+			return redirectOutput;
+			
+		} catch (JsonParseException e) {
+			
 			e.printStackTrace();
-		}
-
-		return null;
-
+		} catch (JsonMappingException e) {
+			
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}	   
+		   
+		   return null;
 	}
+	
 
 }
